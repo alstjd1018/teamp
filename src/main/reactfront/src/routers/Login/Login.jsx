cc95210cdc0e459a7e1631e777402a867d5fa6ab
alstjd1018 <alstjd1018@naver.com>
2023-09-27 09:53:27 +0900

    First commit

--- src/main/reactfront/src/routers/Login/Login.jsx
+++ src/main/reactfront/src/routers/Login/Login.jsx
@@ -1,12 +1,22 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
+
 import "./Login.css";
 
 export default function Login() {
 
     return (
-
-       <div>
-           로그인 화면
-       </div>
+        <div className="App">
+            <a href="http://localhost:8080/find-id/"
+               target="_blank"
+               rel="noopener noreferrer">
+                아이디 찾기이동</a>
+            <a href="http://localhost:8080/find_pass/"
+               target="_blank"
+               rel="noopener noreferrer">
+                비밀번호 찾기이동</a>
+        </div>
+       // <div>
+       //     로그인 화면
+       // </div>
     );
 }
